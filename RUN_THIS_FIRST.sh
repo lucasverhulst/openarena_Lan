72c020183a955543492936d7f9c2c1896c9870e3
Lucas Verhulst <lucasverhulst@hotmail.nl>
2024-10-16 01:34:28 +0200

    added first run shell file.

--- RUN_THIS_FIRST.sh
+++ RUN_THIS_FIRST.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
-if [ "$HAS_RUN"==0 ];
-then
-    # the rest of the commands to run on the first startup go here
-
-    export HAS_RUN=1
+if [ $(id -u) -ne 0 ]
+  then echo zet 'sudo' voor het commando en voer hem opnieuw uit!
+  exit
 fi
 
-echo $HAS_RUN
+echo "dont run this file again"
+
+# the rest of the commands to run on the first startup go here
+# download openarena zip and download docker backup
+wget http://www.openarena.ws/request.php?4 -P ./gameFiles
+
+mkdir dockerImages
+docker pull sago007/openarena:latest
+docker pull crazymax/samba:latest
+
+cd dockerImages
+docker save -o openarena.tar sago007/openarena:latest
+docker save -o samba.tar crazymax/samba:latest
